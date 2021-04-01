e2fc952b8dea6a621c06bf6c6a61bd195382f202
salah7eddine <abousaid7salah@gmail.com>
2021-04-01 20:19:15 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    // return (
+    //   <div className='App'>
+    //     <h1>Hello World</h1>
+    //   </div>
+    // );
+
+    return React.createElement(
+      'div',
+      null,
+      React.createElement('h1', null, 'Hello World !!')
+    );
+  }
+}
+
+export default App;
